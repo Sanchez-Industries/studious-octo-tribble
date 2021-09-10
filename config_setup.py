b9f224199ecaad2b36a8f074ede3e87c3a0e15f1
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-09-10 11:42:39 +0200

    Yes or No ask function

--- config_setup.py
+++ config_setup.py
@@ -6,6 +6,7 @@ from os import system
 from StudiousParasites.StudiousPlayloadInjector import Studious_Playload_Injector as PlayloadInjector
 import argparse
 from pathlib import Path
+from copy import deepcopy
 
 # args Parsing of the command line tool
 parser = argparse.ArgumentParser()
@@ -254,6 +255,76 @@ def find_next_config_number_available(check_numbers_range=(1,1*10**6),modularity
             flag_of_nothing = True
     #
     return {"free_id_number":result_found, "flag_of_nothing":flag_of_nothing, "partially_used_id_numbers":partially_used_id_numbers, "totally_used_id_numbers":totally_used_id_numbers}
+
+#yeah that can be use for any languages ! Enjoy !
+#                                                                                 EN     FR    DE   ES            EN     FR    DE    ES 
+def YesOrNoQuestion( asked_question="Confirmation{YN_default_choice}?",yes_words=["yes","oui","ja","sí"],no_words=["no","non","nein","no"],
+                     YN_default_choice="yes", ask_loop_if_unknow_reply=True, case_sensitive=False, selected_index_reply_lists=None,
+                     no_input_is_default_validation=True, upper_case_forced_wanted_reply = False, full_type_forced_wanted_reply = False
+                   ):
+    O_YN_default_choice = deepcopy(YN_default_choice)
+    while True:
+        if (-1 < selected_index_reply_lists < len(yes_words)) and (-1 < selected_index_reply_lists < len(no_words)):
+            if not full_type_forced_wanted_reply:
+                yes_char = yes_char[selected_index_reply_lists][0]
+                no_char = no_char[selected_index_reply_lists][0]
+        if YN_default_choice == "yes":
+            if upper_case_forced_wanted_reply:
+                yes_char = yes_char.upper()
+                no_char = no_char.lower()
+            else:
+                yes_char = yes_char.lower()
+                no_char = no_char.upper()
+        elif YN_default_choice == "no":
+            if upper_case_forced_wanted_reply:
+                yes_char = yes_char.lower()
+                no_char = no_char.upper()
+            else:
+                no_char = no_char.lower()
+                yes_char = yes_char.upper()
+        #
+        YN_default_choice = "({yes_char}/{no_char})?".format( yes_char = yes_char, no_char = no_char )
+        ask_msg = asked_question.format(YN_default_choice=YN_default_choice)
+        reply = input(ask_msg)
+        if (no_input_is_default_validation) and (len(reply)==0): 
+            if (O_YN_default_choice.lower() == "yes"):
+                reply = True
+            elif (O_YN_default_choice.lower() == "no"):
+                reply = False
+            else:
+                reply = None
+            break
+        if not case_sensitive:
+            reply = reply.lower()
+            YN_default_choice = YN_default_choice.lower()
+        elif case_sensitive:
+            if upper_case_forced_wanted_reply:
+                yes_words = [i.upper() for i in yes_words]
+                no_words = [i.upper() for i in no_words]
+                YN_default_choice = YN_default_choice.upper()
+            if (selected_index_reply_lists != None):
+                if (-1 < selected_index_reply_lists < len(yes_words)) and (-1 < selected_index_reply_lists < len(no_words)):
+                    yes_words, no_words = list( yes_words[selected_index_reply_lists] ), list( no_words[selected_index_reply_lists] )
+        if full_type_forced_wanted_reply:
+            for w in yes_words:
+                if w == reply:
+                    return True
+            for w in no_words:
+                if w == reply:
+                    return False
+        if not full_type_forced_wanted_reply:
+            for w in yes_words:
+                if reply == w[0:len(reply-1)]:
+                    return True
+            for w in no_words:
+                if reply == w[0:len(reply-1)]:
+                    return False
+        else:
+            if not ask_loop_if_unknow_reply: 
+                reply = None 
+                break
+    return reply
+    
 
 # customisation_asks
 def customisation_asks_function():
